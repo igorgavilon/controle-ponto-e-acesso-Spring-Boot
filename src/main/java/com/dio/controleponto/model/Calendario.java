63b69d4603c00604b5825500b2d5f77903e31d52
igorgavilon <igorgavilon@gmail.com>
2021-07-16 21:46:44 -0400

    parte 6: criacao model BancoHoras Movimentacao

--- src/main/java/com/dio/controleponto/model/Calendario.java
+++ src/main/java/com/dio/controleponto/model/Calendario.java
@@ -1,4 +1,18 @@
 package com.dio.controleponto.model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
 public class Calendario {
+    private long id;
+    private TipoData tipoData;
+    private String descricao;
+    private LocalDateTime dataEspecial;
 }
