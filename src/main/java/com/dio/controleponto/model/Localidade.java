aebd32a4d6b97cc34a08a06175fa3e06eb7648f7
igorgavilon <igorgavilon@gmail.com>
2021-07-16 20:34:47 -0400

    parte 3: criação model CategoriaUsuario Empresa Localidade NivelAcesso

---
+++ src/main/java/com/dio/controleponto/model/Localidade.java
@@ -0,0 +1,13 @@
+package com.dio.controleponto.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Localidade {
+    
+}
